150ebad1afc51fe6cec2839c44ae61562b591604
D D Patel <darshandpatel63@gmail.com>
2026-09-25 12:09:18 +0530

    style: format identifier continuation helper

--- compiler/src/lexer.rs
+++ compiler/src/lexer.rs
@@ -344,9 +344,7 @@ fn is_identifier_start(c: char) -> bool {
 }
 
 fn is_identifier_continue(c: char) -> bool {
-    c == '_'
-        || c.is_alphanumeric()
-        || c.is_mark_nonspacing()
+    c == '_' || c.is_alphanumeric() || c.is_mark_nonspacing()
 }
 
 fn keyword(text: &str) -> Option<TokenKind> {
